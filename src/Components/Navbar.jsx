2a8d6f1207b6086cd239b5e2fcb53ae217042d64
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-04 13:32:09 -0700

    Second Commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <h2 className='text-4xl text-yellow-500'>Logo</h2>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
